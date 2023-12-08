72d74d031dc4d21b3bb81885489d8fe88f7a2326
Rushikesh Sasamkar <44942941+Rushikeshsasamkar@users.noreply.github.com>
2023-12-08 16:33:21 +0000

    Arrays

---
+++ Basics/Arrays.js
@@ -0,0 +1,12 @@
+let arr = [1,2,4,5,6]
+// let newArr = arr.join()
+// console.log(typeof newArr);
+
+let newArr2 = arr.splice(1,3);
+console.log(newArr2);
+
+
+let newArr1 = arr.slice(1,3);
+console.log(newArr1);
+
+
---
+++ Basics/NumbersAndMaths.js
@@ -0,0 +1,19 @@
+console.log(Math);
+
+let value = 23.05
+// console.log(value.toFixed());
+
+// let randomVal = Math.floor(Math.random()*10);
+
+// console.log(randomVal);
+
+
+let a =10,b=20
+
+a = a^b
+b = a^b
+a = a^b
+
+console.log(`A is ${a}`);
+
+console.log(`B is ${b}`);
