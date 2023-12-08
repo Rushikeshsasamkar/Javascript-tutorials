a67d5a19f6c6ab551763c0f6ba303d5f7a01b1f3
Rushikesh Sasamkar <44942941+Rushikeshsasamkar@users.noreply.github.com>
2023-12-08 08:59:00 +0000

    heap memories

---
+++ Basics/Memories.js
@@ -0,0 +1,10 @@
+const userOne = {
+    email:"userone@gmail.com",
+    upi:"1@ybl.com"
+}
+
+const userTwo = userOne
+
+userTwo.email = "usertwo@gmail.com"
+
+console.log(userOne);
--- Basics/dataTypes.js
+++ Basics/dataTypes.js
@@ -5,3 +5,12 @@
 
 // Reference
 // Arrays, Objects, Functions
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id==anotherId);  return false
+
+let arr = [1,2,4,5]
+console.log(typeof arr);
+
