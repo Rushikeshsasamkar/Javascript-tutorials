a61e97c294083c3eacbfa84051832dfb8a237be3
Rushikesh Sasamkar <44942941+Rushikeshsasamkar@users.noreply.github.com>
2023-12-08 10:47:55 +0000

    numbers and mathematics

---
+++ Basics/stringMethods.js
@@ -0,0 +1,9 @@
+let url = "https://rushikesh%20sasamkar@gmail.com"
+
+const newUrl = url.replace('%20',' ');
+
+// console.log(newUrl);
+
+const name = "Raj ramesh raut"
+
+console.log(name.split(' '));
